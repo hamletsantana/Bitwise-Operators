e7a639702aa78412b96cc0e390183824d596ae22
Hamlet Santana <danielhamlet123@gmail.com>
2021-06-21 21:12:29 -0400

    Par e Impar

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace Numeric_Functions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Escriba un número: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Es impar? ");
+            Console.WriteLine(Impar(numero));
+            Console.WriteLine("Es par? ");
+            Console.WriteLine(Par(numero));
+        }
+
+        static bool Impar(int num)
+        {
+            int test = 1 & num;
+            if (test == 1)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+            
+        }
+        static bool Par(int num)
+        {
+            int test = 1 & num;
+            if (test == 1)
+            {
+                return false;
+            }
+            else 
+            {
+                return true;
+            }
+            
         }
     }
 }
