0ccdc7d5c9821c71088c5d8824e0fd22023bdca0
Hamlet Santana <danielhamlet123@gmail.com>
2021-06-21 21:00:09 -0400

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Numeric_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
